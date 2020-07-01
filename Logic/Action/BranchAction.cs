165d3c906f4015677f4bdb2d5593a78453175090
Zanges <3868054+Zanges@users.noreply.github.com>
2020-07-01 15:17:20 +0200

    added BranchAction

---
+++ Logic/Action/BranchAction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace InputTweaker.Logic.Action
+{
+    public class BranchAction : ActionBase
+    {
+        private readonly List<ActionBase> _actions;
+        
+        public BranchAction(List<ActionBase> actions)
+        {
+            _actions = actions;
+        }
+        
+        public override void Execute(object input)
+        {
+            foreach (ActionBase action in _actions)
+            {
+                action.Execute(input);
+            }
+        }
+    }
+}
